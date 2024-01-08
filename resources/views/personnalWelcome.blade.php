169979070eda7dab703bd7ab5edb94c2604ff71f
aflori <f.aurelien@yahoo.fr>
2024-01-08 13:41:45 +0100

    step 2.1.3 - modification/new view

---
+++ resources/views/personnalWelcome.blade.php
@@ -0,0 +1,13 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel bis</title>
+    </head>
+    <body>
+    	<p>
+		Hello World From Laravel v10.10 (PHP v8.2.x)
+    	</p>
+    </body>
+</html>
