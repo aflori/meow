999077e06ee5afa83a85c663f793309f52b1a585
aflori <f.aurelien@yahoo.fr>
2024-01-09 11:43:31 +0100

    step 3.4.a - coments table

---
+++ database/migrations/2024_01_09_103011_create_comments.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('coments', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->string("content", 300)->nullable(false);
+            $table->timestamp("creation_date")->nullable(true);
+            $table->timestamp("modification_date")->nullable(true);
+            $table->uuid('meows_id');
+
+            $table->primary('id');
+            $table->foreign('meows_id')->references('id')->on('meows');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('coments');
+    }
+};
